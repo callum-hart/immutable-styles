90f84d0052af1cc34f5259449dfb88702a2891c8
Callum Hart <callum@callumhart.com>
2018-11-26 21:31:25 +0000

    Add html-examples package, rename examples package to react-examples

--- packages/react-examples/src/App.jsx
+++ packages/react-examples/src/App.jsx
@@ -4,13 +4,11 @@ import "./App.iss.jsx";
 
 import SharingStylesWithMixins from './sharingStylesWithMixins/SharingStylesWithMixins';
 // import SharingStylesWithDetachedRulesets from './sharingStylesWithDetachedRulesets/SharingStylesWithDetachedRulesets';
-// import GroupedSelectors from './groupedSelectors/GroupedSelectors';
 
 const App = () => (
   <section className="app">
     <SharingStylesWithMixins />
     {/* <SharingStylesWithDetachedRulesets /> */}
-    {/* <GroupedSelectors /> */}
   </section>
 )
 
